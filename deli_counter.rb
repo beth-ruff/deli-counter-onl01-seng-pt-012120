5be8a18987abff55ae2f0a81d876820f666246a5
Beth Ruff <beth.ruff2@gmail.com>
2020-01-31 00:52:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(deli)
 end
 
 def take_a_number(deli, person)
-  puts "Welcome, #{person}. You are number #{deli.with_index(1)} in line."
+  puts "Welcome, #{person}. You are number #{deli.length} in line."
 end
 
   
