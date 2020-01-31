6ddf1ab56f70f541ac43c875534c37063b7db97a
Beth Ruff <beth.ruff2@gmail.com>
2020-01-31 00:24:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
+    str = "The line is currently:"
     deli.each.with_index(1) do |person, i|
-      puts "The line is currently: #{i}, #{person}"
+      str += "#{i}, #{person}"
     end
+    str
   end
 end
 
