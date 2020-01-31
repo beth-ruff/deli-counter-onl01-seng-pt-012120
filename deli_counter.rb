460d03de9d0eeaefa6c14f0d823f56afbbbcc882
Beth Ruff <beth.ruff2@gmail.com>
2020-01-31 00:09:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    deli.each.with_index(1) |person, i|
+    deli.each.with_index(1) do |person, i|
       puts "The line is currently: #{i}, #{person}"
   end
 end
